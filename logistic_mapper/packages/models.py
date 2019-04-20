9ccea1fc9b4f130c27447f1545a6de379ab36ca3
Renan Marinho Fernandes <renansycer@outlook.com>
2019-04-20 18:01:36 -0300

    criando classe Package

--- logistic_mapper/packages/models.py
+++ logistic_mapper/packages/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Package (models.Model):
+    name = models.CharField(max_length = 200)
+    pol = models.CharField(max_lenght = 200) #posiçao atual
+    pos = models.CharField(max_lenght = 200) #desitno
+    weight = models.FloatField(default = 0)
+
+    def __init__(self, name,pol,pos,weight):
+        self.name =  name
+        self.pol = pol
+        self.pos = pos
+        self.weight = weight
+    
+    def getName()
+        return self.name
+
+    def getPol()
+        return self.pol
+    
+    def getPos()
+        return self.pos
+    
+    def getWeight()
+        return self.weight
+
+    
